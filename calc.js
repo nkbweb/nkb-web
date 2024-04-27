972a4cd66313bb9b9ba076d7925dc0f1792877ea
nkbweb <168272378+nkbweb@users.noreply.github.com>
2024-04-27 16:09:10 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,22 @@
+//calc
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+   display.value+=input;
+}
+
+
+function clearScreen(){
+    display.value='';
+}
+
+
+function calculate(){
+    try{
+      display.value=eval(display.value);
+     }
+    catch(error){
+        display.value='ERROR';
+    }
+} 
